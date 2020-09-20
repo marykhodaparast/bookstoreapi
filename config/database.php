1286831f8d93b95aed6adccd6f9a76b1e71054df
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-20 13:06:31 +0430

    use Multiple database connection-(version1)

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+
+    'default' => 'mysql',
+
+    'connections' => [
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+
+];
